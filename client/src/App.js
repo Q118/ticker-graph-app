126b474a537e88aa1b822674e8dfa3bd520c63b9
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-02-09 13:55:49 -0800

    form/chart rendering to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import Chart from './components/Chart';
+import Form from './components/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container fluid>
+            <div className="App">
+                <Row>
+                    <Form />
+                </Row>
+                <Row>
+                    <Chart />
+                </Row>
+            </div>
+        </Container>
+    );
 }
 
 export default App;
