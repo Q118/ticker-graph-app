311b9626c95831f4196eaf8101b280ebf021fda2
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-02-09 17:22:59 -0800

    Updates

---
+++ precache-manifest.58a1df2d484bbedcf15ac24425312773.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303a0cf77621148d423dc75200f7f380",
+    "url": "/ticker-graph-app/index.html"
+  },
+  {
+    "revision": "ed66c2ab87899f793438",
+    "url": "/ticker-graph-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f6fa20861c667d29d9ac",
+    "url": "/ticker-graph-app/static/css/main.f8d13acc.chunk.css"
+  },
+  {
+    "revision": "ed66c2ab87899f793438",
+    "url": "/ticker-graph-app/static/js/2.e7a462f1.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ticker-graph-app/static/js/2.e7a462f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6fa20861c667d29d9ac",
+    "url": "/ticker-graph-app/static/js/main.2d5b79c3.chunk.js"
+  },
+  {
+    "revision": "c3141b56a014a702c251",
+    "url": "/ticker-graph-app/static/js/runtime-main.b9c79ac5.js"
+  },
+  {
+    "revision": "daf066334c2ea9544a417787bcabd344",
+    "url": "/ticker-graph-app/static/media/mock-background.daf06633.png"
+  }
+]);
